cac32dffd5112897f56de20baec626cefc77f166
Ali Habib <alihabib2005@hotmail.com>
2021-04-22 02:25:18 +0300

    initial commit empty project

---
+++ vendor/composer/InstalledVersions.php
@@ -0,0 +1,559 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+namespace Composer;
+
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => '1.2.x-dev',
+    'version' => '1.2.9999999.9999999-dev',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'laminas/laminas-mvc-skeleton',
+  ),
+  'versions' => 
+  array (
+    'brick/varexporter' => 
+    array (
+      'pretty_version' => '0.3.3',
+      'version' => '0.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '736378d357a189a692390bbc9e0eed835b079805',
+    ),
+    'container-interop/container-interop' => 
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '79cbf1341c22ec75643d841642dd5d6acd83bdb8',
+    ),
+    'container-interop/container-interop-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '^1.2',
+      ),
+    ),
+    'laminas/laminas-component-installer' => 
+    array (
+      'pretty_version' => '2.4.0',
+      'version' => '2.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '98c6590594b5a61710a5b5119edb93d2fc66e090',
+    ),
+    'laminas/laminas-config' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0bce6f5abab41dc673196741883b19018a2b5994',
+    ),
+    'laminas/laminas-development-mode' => 
+    array (
+      'pretty_version' => '3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '11b2adc8837e4419a5b31e2a7ae59f06636d4096',
+    ),
+    'laminas/laminas-escaper' => 
+    array (
+      'pretty_version' => '2.7.0',
+      'version' => '2.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e04bc5ae5990b17159d79d331055e2c645e5cc5',
+    ),
+    'laminas/laminas-eventmanager' => 
+    array (
+      'pretty_version' => '3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1940ccf30e058b2fd66f5a9d696f1b5e0027b082',
+    ),
+    'laminas/laminas-http' => 
+    array (
+      'pretty_version' => '2.14.1',
+      'version' => '2.14.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e2796b61dd6c8353084f0e821111af864ab3bbbe',
+    ),
+    'laminas/laminas-json' => 
+    array (
+      'pretty_version' => '3.1.2',
+      'version' => '3.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '00dc0da7b5e5018904c5c4a8e80a5faa16c2c1c6',
+    ),
+    'laminas/laminas-loader' => 
+    array (
+      'pretty_version' => '2.6.1',
+      'version' => '2.6.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5d01c2c237ae9e68bec262f339947e2ea18979bc',
+    ),
+    'laminas/laminas-modulemanager' => 
+    array (
+      'pretty_version' => '2.10.1',
+      'version' => '2.10.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '637aaaf2c85d13694b096e253e5884653f93bb92',
+    ),
+    'laminas/laminas-mvc' => 
+    array (
+      'pretty_version' => '3.2.0',
+      'version' => '3.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '88da7200cf8f5a970c35d91717a5c4db94981e5e',
+    ),
+    'laminas/laminas-mvc-skeleton' => 
+    array (
+      'pretty_version' => '1.2.x-dev',
+      'version' => '1.2.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'laminas/laminas-router' => 
+    array (
+      'pretty_version' => '3.4.4',
+      'version' => '3.4.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2a7068508af4de67d80ea292e0cc7c37563a33c6',
+    ),
+    'laminas/laminas-servicemanager' => 
+    array (
+      'pretty_version' => '3.5.1',
+      'version' => '3.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0d4c8628a71fae9f7bd0b1b74b76382e5e9a04b1',
+    ),
+    'laminas/laminas-skeleton-installer' => 
+    array (
+      'pretty_version' => '0.2.2',
+      'version' => '0.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6e8c0044290ef2ce152a8d75288497449c4620e4',
+    ),
+    'laminas/laminas-stdlib' => 
+    array (
+      'pretty_version' => '3.3.1',
+      'version' => '3.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd81c7ffe602ed0e6ecb18691019111c0f4bf1efe',
+    ),
+    'laminas/laminas-uri' => 
+    array (
+      'pretty_version' => '2.8.0',
+      'version' => '2.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8651611b6285529f25a4cb9a466c686d9b31468e',
+    ),
+    'laminas/laminas-validator' => 
+    array (
+      'pretty_version' => '2.13.4',
+      'version' => '2.13.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '93593684e70b8ed1e870cacd34ca32b0c0ace185',
+    ),
+    'laminas/laminas-view' => 
+    array (
+      'pretty_version' => '2.12.0',
+      'version' => '2.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ef103da6887809f08ecf52f42c31a76c9bf08b1',
+    ),
+    'laminas/laminas-zendframework-bridge' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ede70583e101030bcace4dcddd648f760ddf642',
+    ),
+    'nikic/php-parser' => 
+    array (
+      'pretty_version' => 'v4.10.4',
+      'version' => '4.10.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6d052fc58cb876152f89f532b95a8d7907e7f0e',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'psr/container-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '^1.0',
+      ),
+    ),
+    'webimpress/safe-writer' => 
+    array (
+      'pretty_version' => '2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5cfafdec5873c389036f14bf832a5efc9390dcdd',
+    ),
+    'zendframework/zend-component-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.1.2',
+      ),
+    ),
+    'zendframework/zend-config' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^3.3.0',
+      ),
+    ),
+    'zendframework/zend-escaper' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.6.1',
+      ),
+    ),
+    'zendframework/zend-eventmanager' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^3.2.1',
+      ),
+    ),
+    'zendframework/zend-http' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.11.2',
+      ),
+    ),
+    'zendframework/zend-json' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '3.1.2',
+      ),
+    ),
+    'zendframework/zend-loader' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '2.6.1',
+      ),
+    ),
+    'zendframework/zend-modulemanager' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.8.4',
+      ),
+    ),
+    'zendframework/zend-mvc' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^3.1.1',
+      ),
+    ),
+    'zendframework/zend-router' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^3.3.0',
+      ),
+    ),
+    'zendframework/zend-servicemanager' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^3.4.0',
+      ),
+    ),
+    'zendframework/zend-skeleton-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^0.1.7',
+      ),
+    ),
+    'zendframework/zend-stdlib' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^3.2.1',
+      ),
+    ),
+    'zendframework/zend-uri' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.7.1',
+      ),
+    ),
+    'zendframework/zend-validator' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.13.0',
+      ),
+    ),
+    'zendframework/zend-view' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.11.4',
+      ),
+    ),
+    'zfcampus/zf-development-mode' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^3.2.0',
+      ),
+    ),
+  ),
+);
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+return array_keys(self::$installed['versions']);
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+return isset(self::$installed['versions'][$packageName]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+$ranges = array();
+if (isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = self::$installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['version'];
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['pretty_version'];
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['reference'];
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+return self::$installed['root'];
+}
+
+
+
+
+
+
+
+public static function getRawData()
+{
+return self::$installed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+}
+}
