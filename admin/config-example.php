2103cb80b41ef5d22a65eb1d0a5c3610b66d1fdc
Ali Habib <alihabib2005@hotmail.com>
2021-07-04 00:20:51 +0300

    setup auto deploy

--- admin/config-example.php
+++ admin/config-example.php
@@ -9,7 +9,7 @@ $simpleImage = new SimpleImage();
 
 $siteName = "Mastershop";
 $siteLink = "http://localhost/mastershop/";
-$imagesLink = $siteLink . 'public/uploads/images/';
+$imagesLink = $siteLink.'public/uploads/images/';
 
 // default images path
 $imagesPath = "../public/uploads/images/";
@@ -29,11 +29,11 @@ define('AUDIO_PATH', $audioPath);
 
 // images different dimension variables
 $regularImageW = 800;
-$regularImageH = 600;
+$regularImageH= 600;
 $medImageW = 400;
 $medImageH = 355;
 $smallImageW = 150;
 $smallImageH = 100;
 
 
-$fileTypes = array(".xls", ".xlsx", ".doc", ".docx", ".pdf", ".txt", ".rtf", ".zip", ".flv", ".gif", ".jpg", ".jpeg", ".png", ".mp3", ".wav", ".wave", ".ram", ".mpeg", ".mp4");
+$fileTypes = array(".xls",".xlsx",".doc",".docx",".pdf",".txt",".rtf",".zip",".flv",".gif",".jpg",".jpeg",".png", ".mp3", ".wav", ".wave", ".ram", ".mpeg", ".mp4");
